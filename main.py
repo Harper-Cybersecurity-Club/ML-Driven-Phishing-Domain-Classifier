9145f3c5f80e77020a6a56fc59c6308ff443985a
Phil Samuel <mf98604@mail.harpercollege.edu>
2026-01-28 14:48:22 -0600

    Add initial structure for phishing domain classifier

---
+++ main.py
@@ -0,0 +1,16 @@
+# ML Driven Phishing Domain Classifier
+
+# EXTRACT FEATURES
+
+def extract_features(domain):
+    pass
+
+"""
+
+LENGTH - Longer domain length corresponds with likely phishing
+DOMAIN CREATION DATE - Older domain creation date corresponds with likely legitimate
+CHARACTER ENTROPY (RANDOMNESS) - Greater domain character entropy corresponds with likely phishing
+
+BLACKLISTS - Domain listed on blacklist corresponds with likely phishing
+
+"""
